e88507f7b996866b0cd14ef84a132f1e4f992d2f
samettopcu88 <samet.topcu.88@icloud.com>
2024-11-10 21:35:42 +0300

    Kullanıcı tablosu oluşturuldu.

--- DatabaseApp/Form1.Designer.cs
+++ DatabaseApp/Form1.Designer.cs
@@ -41,14 +41,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(502, 126);
+            this.textBox1.Location = new System.Drawing.Point(506, 166);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(502, 181);
+            this.textBox2.Location = new System.Drawing.Point(506, 217);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
@@ -56,20 +57,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(358, 132);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(346, 167);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.Size = new System.Drawing.Size(74, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "Kullanıcı Adı";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(361, 187);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(349, 222);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(43, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "Parola";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -78,18 +79,19 @@
             // 
             this.button1.BackgroundImage = global::DatabaseApp.Properties.Resources.Bouton_Login;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(361, 249);
+            this.button1.Location = new System.Drawing.Point(425, 295);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 38);
             this.button1.TabIndex = 5;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DatabaseApp.Properties.Resources.pngegg;
-            this.pictureBox1.Location = new System.Drawing.Point(52, 82);
+            this.pictureBox1.Location = new System.Drawing.Point(70, 102);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(251, 205);
+            this.pictureBox1.Size = new System.Drawing.Size(223, 231);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -98,11 +100,12 @@
             // 
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.Location = new System.Drawing.Point(502, 249);
+            this.button2.Location = new System.Drawing.Point(563, 295);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(43, 38);
             this.button2.TabIndex = 6;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -117,6 +120,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
--- DatabaseApp/Form1.cs
+++ DatabaseApp/Form1.cs
@@ -21,5 +21,20 @@ namespace DatabaseApp
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.AcceptButton = button1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.CancelButton = button2;
+        }
     }
 }
