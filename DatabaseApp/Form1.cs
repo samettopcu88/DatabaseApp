1b5830a4308f81f5e3d08eeea437c07473152969
samettopcu88 <samet.topcu.88@icloud.com>
2024-11-13 04:50:24 +0300

    Çıkış butonu ve kullanıcı giriş ile icerik formuna ulaşma kısmı yapıldı.

--- DatabaseApp/Form1.cs
+++ DatabaseApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DatabaseApp
 {
@@ -29,11 +30,35 @@ namespace DatabaseApp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            SqlConnection baglanti = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DatabaseApp.mdf;Integrated Security=True");
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from kullanici where kullaniciadi=@kullaniciadi and parola=@parola", baglanti);
+            komut.Parameters.AddWithValue("@kullaniciadi", textBox1.Text);
+            komut.Parameters.AddWithValue("@parola", textBox2.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                icerik frm = new icerik();
+                frm.Show();
+                this.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Yanlış kullanıcı adı veya parolası", "Uyarı");
+            }
+            
             this.AcceptButton = button1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            DialogResult onay = MessageBox.Show("Çıkmak istediğinize emin misini?", "Çıkış İşlemi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (onay == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+            
             this.CancelButton = button2;
         }
     }
